21ef7c472fab875eb7b8cc014ce70e99982b273a
Jonathan Woodbury <jpwoodbu@gmail.com>
2026-01-18 15:33:57 -0500

    Fix typo

--- src/wyoming_nemo_parakeet/main.py
+++ src/wyoming_nemo_parakeet/main.py
@@ -38,7 +38,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def start() -> None:
     # The handler seems to get re-created for every request, so we need to do
-    # the expensive work of loading the model ahead and time and pass it in.
+    # the expensive work of loading the model ahead of time and pass it in.
     model = asr.models.ASRModel.from_pretrained(
         model_name=f"{handler.REPO_ID}/{handler.MODEL_ID}"
     )
